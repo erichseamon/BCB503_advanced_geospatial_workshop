4b1b397463271cb7c8456db5c3d4c5bfbf82f7dc
Erich Seamon <erichs@zaphod2.ibest.uidaho.edu>
2021-04-17 13:14:08 -0700

    added usePackage to library test

--- src/day1-00-prep-librarytest.R
+++ src/day1-00-prep-librarytest.R
@@ -1,32 +1,37 @@
 #dyn.load("/opt/modules/climatology/gdal/3.0.2/lib/libgdal.so")
-#library(sf, lib="/mnt/lfs2/erichs/R/x86_64-pc-linux-gnu-library/3.6/")
+#usePackage(sf, lib="/mnt/lfs2/erichs/R/x86_64-pc-linux-gnu-usePackage/3.6/")
+usePackage <- function(p) {
+  if (!is.element(p, installed.packages()[,1]))
+    install.packages(p, dep = TRUE, repos = "http://cran.us.r-project.org")
+  require(p, character.only = TRUE)
+}
 
-library(sf)
-library(raster)
-library(rgdal)
-library(ggplot2)
-library(dplyr)
-library(sp)           ## Data management
-library(spdep)        ## Spatial autocorrelation
-library(gstat)        ## Geostatistics
-library(splancs)      ## Kernel Density
-library(spatstat)     ## Geostatistics
-library(pgirmess)     ## Spatial autocorrelation
-library(RColorBrewer) ## Visualization
-library(classInt)     ## Class intervals
-library(broom)        # contains the tidy function which now replaces the fortify function for ggplot
-library(viridis)      # For nicer ggplot colours
-library(gridExtra)    # Multiple plot
-library(spatialreg)
-library(maptools)
-library(plyr)
-library(gstat)
-library(car)
-library(RStoolbox)
-library(caret)
-library(caretEnsemble)
-library(doParallel)
-library(randomForest) # Random Forest
-library(GWmodel)      ### GW models
-library(gtable)
-library(SpatialML)    # Geographically weighted regression
+usePackage("sf")
+usePackage("raster")
+usePackage("rgdal")
+usePackage("ggplot2")
+usePackage("dplyr")
+usePackage("sp")           ## Data management
+usePackage("spdep")        ## Spatial autocorrelation
+usePackage("gstat")        ## Geostatistics
+usePackage("splancs")      ## Kernel Density
+usePackage("spatstat")     ## Geostatistics
+usePackage("pgirmess")     ## Spatial autocorrelation
+usePackage("RColorBrewer") ## Visualization
+usePackage("classInt")     ## Class intervals
+usePackage("broom")        # contains the tidy function which now replaces the fortify function for ggplot
+usePackage("viridis")      # For nicer ggplot colours
+usePackage("gridExtra")    # Multiple plot
+usePackage("spatialreg")
+usePackage("maptools")
+usePackage("plyr")
+usePackage("gstat")
+usePackage("car")
+usePackage("RStoolbox")
+usePackage("caret")
+usePackage("caretEnsemble")
+usePackage("doParallel")
+usePackage("randomForest") # Random Forest
+usePackage("GWmodel")      ### GW models
+usePackage("gtable")
+usePackage("SpatialML")    # Geographically weighted regression
